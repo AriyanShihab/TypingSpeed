1bac643d97c48a35d23f1821c56a35643199829d
AriyanShihab <devshihab111@gmail.com>
2022-09-14 20:53:30 +0600

    requerments file added

---
+++ requerments.js
@@ -0,0 +1,40 @@
+/*
+
+submission dedline for 60 marks: ( all the times are standerd BD time)
+15-09-2022 11:59PM
+submission dedline for 50 marks
+17-09-2022 11:59PM
+    
+
+
+
+compelsury:
+
+    1: fix the start button;
+    2: display count down ( just use bactice in template string);
+    3: remove the overlay after the countdown   hasbeen zero;
+    4: place the summary modal middle of screen;
+    5: in result section:
+        in lerger divice the resul will be in the 3 column and in mobile divice it will be in single column
+    6: time has to in intiger. ( nothing after  the decimal, not even the decimal)
+
+
+for bonous:
+
+    1: count the errors and display in the result popup
+    2: make at-least 10 github commit
+    3: create a  blog.html file and answer the given questions bellow in that file;
+     questions:
+        a: difarence between localStorage and Session Storage
+        b: dif between global scope and local scope
+        3: tell me some ways of getting undifined value
+
+
+what to submit:
+a: private repo link;
+b: live website deployed on netlify
+
+
+
+
+*/
